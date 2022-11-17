0a76c551380a32c4914d2b0bff0d879a6c8ba1f0
getRicha <richasri2002@gmail.com>
2022-11-17 16:31:28 +0530

    Add map for selecting location

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import { useEffect, useState } from "react";
 import './App.css';
-import logo from './mlh-prep.png'
+import logo from './mlh-prep.png';
+import Map from "./components/Map";
 
 function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [city, setCity] = useState("New York City")
   const [results, setResults] = useState(null);
+  const [cityCoordinates, setCityCoordinates] = useState({
+    lat: 51.505,
+    lon: -0.09,
+  });
 
   useEffect(() => {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&appid=" + process.env.REACT_APP_APIKEY)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_APIKEY}`)
       .then(res => res.json())
       .then(
         (result) => {
@@ -18,6 +23,10 @@ function App() {
           } else {
             setIsLoaded(true);
             setResults(result);
+            setCityCoordinates({
+              lat: result.coord.lat,
+              lon: result.coord.lon,
+            });
           }
         },
         (error) => {
@@ -46,6 +55,13 @@ function App() {
             <p>Feels like {results.main.feels_like}°C</p>
             <i><p>{results.name}, {results.sys.country}</p></i>
           </>}
+        </div>
+        <div className="weather-map">
+          <Map 
+            city={city}
+            setCity={setCity}
+            cityCoordinates={cityCoordinates}
+            setCityCoordinates={setCityCoordinates}/>
         </div>
       </div>
     </>
---
+++ src/components/Map.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react';
+import leaflet from 'leaflet';
+import {
+    MapContainer,
+    TileLayer,
+    Marker,
+    Popup,
+    useMapEvent,
+} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+const DefaultIcon = leaflet.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+});
+
+leaflet.Marker.prototype.options.icon = DefaultIcon;
+
+const SetMarkerDynamically = ({
+    city,
+    setCity,
+    cityCoordinates,
+    setCityCoordinates,
+}) => {
+  
+    useMapEvent('click', (e) => {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${e.latlng.lat}&lon=${e.latlng.lng}&appid=${process.env.REACT_APP_APIKEY}`
+      )
+        .then((res) => res.json())
+        .then((res) => {
+          setCityCoordinates({ lat: e.latlng.lat, lon: e.latlng.lng });
+          setCity(res.name);
+        });
+    });
+  
+    return (
+      <Marker position={[cityCoordinates.lat, cityCoordinates.lon]}>
+        <Popup>{city}</Popup>
+      </Marker>
+    );
+};
+
+const Map = ({ city, setCity, cityCoordinates, setCityCoordinates }) => {
+    const [map, setMap] = useState();
+  
+    useEffect(() => {
+      setMap(map);
+    }, [map]);
+  
+    useEffect(() => {
+      const mapCenter = [cityCoordinates.lat, cityCoordinates.lon];
+      if (map) {
+        if (map.getZoom() < 4) {
+          map.setView(mapCenter, 7);
+        } else {
+          map.setView(mapCenter, map.getZoom());
+        }
+      }
+    }, [map, cityCoordinates, city]);
+  
+    return (
+        <MapContainer
+            whenCreated={setMap}
+            center={[cityCoordinates.lat, cityCoordinates.lon]}
+            scrollWheelZoom={true}
+            zoom={7}
+            style={{width:'100%',height:'100vh',borderRadius:'10px'}}
+        >
+        <TileLayer
+            attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <SetMarkerDynamically
+            city={city}
+            setCity={setCity}
+            cityCoordinates={cityCoordinates}
+            setCityCoordinates={setCityCoordinates}
+        />
+        </MapContainer>
+    );
+};
+  
+export default Map;
+
